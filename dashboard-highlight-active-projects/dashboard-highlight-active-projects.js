25f7a62ff88e15046f88f01436025841e5294e62
Christian Schuerer-Waldheim <csw@gmx.at>
2014-10-05 20:57:46 +0200

    Add TestRail UI Script
    This UI Script highlights projects on the Dashboard, which have active
    test runs.

---
+++ dashboard-highlight-active-projects/dashboard-highlight-active-projects.js
@@ -0,0 +1,23 @@
+name: Dashboard - Highlight active projects (Compact View only)
+description: See https://github.com/quincunx/testrail-ui-scripts
+author: Christian Schuerer-Waldheim <csw@gmx.at>
+version: 1.0
+includes: ^dashboard
+excludes: 
+
+js:
+$(document).ready(
+  function() {
+    // Check if Compact View is being displayed
+    if ($('span.content-header-icon:nth-child(1) > a:nth-child(1) > img:nth-child(1)').attr('src').indexOf("Inactive") >= 0) {
+      console.log("TestRail UI Script - Dashboard - Highlight active projects: Only Compact View is supported");
+    } else {
+      $('#content-inner > table > tbody  > tr').each(function() {
+	var $project = $(this);
+	var run_count = 0;
+	$project.children('td:nth-child(3)').each(function() { run_count = $(this).children('strong:nth-child(3)').html(); });
+	if (run_count > 0) { $project.css("background-color", "#D1E6FD"); }
+      });
+    }
+  }
+);
